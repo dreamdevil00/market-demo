1690467aa82b33194e23ad3237a98c0ae494dff3
dreamdevil00 <dreamdeviloo@163.com>
2018-07-10 17:30:19 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable*/
+
+import './main.scss';
+
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+
+function component() {
+  const element = document.createElement('button');
+  element.id = 'btn'
+  element.classList = "btn btn-primary";
+  element.innerHTML = 'Click Me';
+  return element;
+}
+
+$('body').append(component());
+$('#btn').click(() => {
+  alert('Hello webpack!');
+});
